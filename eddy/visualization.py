2a53057dd7a750a5853b6e495e73991981bc0cf4
Julian Stier <mail@julian-stier.de>
2020-02-05 15:14:08 +0100

    Testing visualizing covariances of groups

--- eddy/visualization.py
+++ eddy/visualization.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+import mpl_toolkits.mplot3d.art3d as art3d
 from mpl_toolkits.mplot3d import Axes3D
 
 from .objective import Objective
@@ -45,12 +48,13 @@ def adjust_lightness(color, amount=0.5):
 def obtain_path_centroid(x, objective: Objective=None):
     """
 
-    :param x: d-dimensional points of amount n with shape (d,n)
+    :param x: d-dimensional points of amount n with shape (n,d)
     :return: Single d-dimensional point (d,)
     """
     assert len(x.shape) == 2
-    num_points = x.shape[1]
-    return np.sum(x, axis=1)/num_points
+    #num_points = x.shape[0]
+    #return np.sum(x, axis=0)/num_points
+    return np.mean(x, axis=0)
 
 
 def obtain_first_element(x, objective: Objective=None):
@@ -60,38 +64,165 @@ def obtain_first_element(x, objective: Objective=None):
     :return: Single d-dimensional point (d,)
     """
     assert len(x.shape) == 2
-    return x[:,0]
+    return x[0]
 
 
-def obtain_maximum_objective(x, objective: Objective=None):
+def obtain_extremum_objective(x, objective: Objective, comp=np.greater):
     """
 
     :param x: d-dimensional points of amount n with shape (d,n)
     :return: Single d-dimensional point (d,)
     """
     assert len(x.shape) == 2
-    return np.maximum([objective.evaluate_visual(p) for p in x])
+    extremum_x = x[0]
+    extremum_z = objective.evaluate_visual(x[0])
+    for p in x[1:]:
+        z = objective.evaluate_visual(p)
+        if comp(z, extremum_z):
+            extremum_x = p
+            extremum_z = z
+    return extremum_x
+
+
+def obtain_maximum_objective(x, objective: Objective):
+    return obtain_extremum_objective(x, objective)
+
+
+def obtain_minimum_objective(x, objective: Objective):
+    return obtain_extremum_objective(x, objective, comp=np.smaller)
 
 
 fn_path_centroid_selectors = {
     'centroid': obtain_path_centroid,
     'first': obtain_first_element,
-    'max': obtain_maximum_objective
+    'max': obtain_maximum_objective,
+    'min': obtain_minimum_objective
 }
 
 
-def visualize_path(search_path, objective: Objective = None, axis=None, connected_path=True, path_centroid_aggregate='centroid'):
+def old_visualize_path(search_path, objective: Objective = None, space: int=None, infer_objective: bool=True, axis=None, connected_path=True, path_centroid_aggregate='centroid'):
     assert hasattr(search_path, 'shape'), 'Visualization of path needs a shape information for accessing dimension sizes'
-    assert search_path.shape[0] == 2 or search_path.shape[0] == 3, 'We expect the first dimension of the search path to match the dimensions of the points. Currently only 2d and 3d is supported'
+    assert search_path.shape[-1] == 2 or search_path.shape[-1] == 3, 'We expect the last dimension of the search path to match the dimensions of the points. Currently only 2d and 3d is supported'
 
-    num_dims = search_path.shape[0]
-    num_points = search_path.shape[-1]
+    num_dims = search_path.shape[-1]
+    num_points = search_path.shape[-2]
+
+    infer_objective = infer_objective and objective is not None
+    if space is None:
+        # Space will contain the dimensional space we are in with our points, e.g. 1D or 2D
+        if num_dims == 2:
+            if not infer_objective:
+                space = 1  # we have no objective (or mustnt use it) and only a tuple (x1, x2), so x1 must be the point and x2 the objective
+            else:
+                space = 2   # we have an objective available, so we can infer a third dimension for the objective. (x1, x2) -> (z)
+        elif num_dims == 3:
+            space = 2  # we have two points (x1, x2) available and use the third (.,.,x3) as z and might use the objective for extra interpolation
 
     if axis is None:
         fig = plt.figure()
         axis = fig.gca(projection='3d')
 
     if connected_path:
-        fn_path_centroid = None
-        if path_centroid_aggregate == 'centroid':
-            fn_path_centroid
+        if len(search_path.shape) > 2:
+            # We have an extra dimension preceeding (n,d) and call it "g" for generations or populations: (?,g,n,d)
+            fn_path_centroid = fn_path_centroid_selectors[path_centroid_aggregate] if path_centroid_aggregate in fn_path_centroid_selectors else path_centroid_aggregate
+            assert fn_path_centroid != None
+
+            # Compute centroids along the second last axis (num_points) and reduce (?,?,g,n,d) to (?,?,g,d)
+            centroids = np.array([fn_path_centroid(g.reshape(-1, g.shape[-1]), objective) for g in search_path.reshape(-1, search_path.shape[-2], search_path.shape[-1])]).reshape(np.delete(search_path.shape, len(search_path.shape) - 2, 0))
+
+            # Now we want to flatten all dimensions from (?,?,g,d) to (?,g,d) to treat each path of (,d) as grouped paths we will plot together
+            grouped_centroid_paths = centroids.reshape(-1, centroids.shape[-2], centroids.shape[-1])
+            #print('Grouped centroid paths shape', grouped_centroid_paths.shape)
+            for group, group_color in zip(grouped_centroid_paths, ['g', 'y', 'b']):
+                # Each point in group is now d-dimensional
+                for ix in range(group.shape[-2]-1):
+                    xs = group[ix:ix+2,0]
+                    ys = group[ix:ix+2,1]
+                    zs = [objective.evaluate_visual([px, py]) for px, py in zip(xs, ys)]
+                    axis.plot(xs, ys, zs, ls='--', lw=0.8, color=group_color)
+                    #axis.scatter(np.concatenate([p1, [objective.evaluate_visual(p1)]]), np.concatenate([p2, [objective.evaluate_visual(p2)]]), marker='>', color=group_color)
+
+
+def visualize_path(search_path, objective: Objective = None, axis=None, path_centroid_aggregate='centroid', group_level: int=1):
+    if hasattr(search_path, 'shape'):
+        print(search_path.shape)
+
+        if len(search_path.shape) > 2:
+            print('a')
+            fn_path_centroid = fn_path_centroid_selectors[path_centroid_aggregate] if path_centroid_aggregate in fn_path_centroid_selectors else path_centroid_aggregate
+            assert fn_path_centroid != None
+
+            # Compute centroids along the second last axis (num_points) and reduce (?,?,g,n,d) to (?,?,g,d)
+            centroids = np.array([fn_path_centroid(g.reshape(-1, g.shape[-1]), objective) for g in search_path.reshape(-1, search_path.shape[-2], search_path.shape[-1])]).reshape(np.delete(search_path.shape, len(search_path.shape) - 2, 0))
+            grouped_centroid_paths = centroids.reshape(-1, centroids.shape[-2], centroids.shape[-1])
+
+            centroids = []
+            covariances = []
+            for group in grouped_centroid_paths:
+                centroid, covariance = visualize_group(search_path, axis, objective=objective)
+                centroids.append(centroid)
+                covariances.append(covariances)
+
+            return np.mean(centroids), None
+
+        else:
+            print('b')
+            return visualize_group(search_path, axis, objective=objective)
+
+    else:
+        # Unpack a list of different-sized groups
+        centroids = []
+        covariances = []
+        for group in search_path:
+            centroid, covariance = visualize_group(group, axis, plot_covariance=True, objective=objective)
+            centroids.append(centroid)
+            covariances.append(covariance)
+        centroids = np.array(centroids)
+        covariances = np.array(covariances)
+
+        """group_color = 'b'
+        for ix in range(centroids.shape[-2] - 1):
+            xs = centroids[ix:ix + 2, 0]
+            ys = centroids[ix:ix + 2, 1]
+            zs = [objective.evaluate_visual([px, py]) for px, py in zip(xs, ys)]
+            axis.plot(xs, ys, zs, ls='--', lw=0.8, color=group_color)"""
+        return visualize_group(centroids, axis, connect_pairwise=True, objective=objective, group_color='b')
+        #return np.mean(centroids, axis=0)
+
+
+def visualize_group(group, axis, connect_pairwise=False, plot_covariance=False, objective: Objective = None, group_color='b'):
+    assert hasattr(group, 'shape')
+    assert len(group.shape) == 2
+    assert group.shape[1] == 2 or group.shape[1] == 3
+
+    num_dims = group.shape[1]
+    num_points = group.shape[0]
+
+    centroid = np.mean(group, axis=0)
+    covariance = np.cov(group.T)
+
+    if connect_pairwise:
+        for ix in range(num_points - 1):
+            xs = group[ix:ix + 2, 0]
+            ys = group[ix:ix + 2, 1]
+            if num_dims == 2:
+                zs = [objective.evaluate_visual([px, py]) for px, py in zip(xs, ys)]
+            else:
+                zs = group[ix:ix + 2, 2]
+            axis.plot(xs, ys, zs, ls='--', lw=0.8, color=group_color)
+
+    if plot_covariance and num_points > num_dims * 2:
+        n_std = 3.0
+        pearson = covariance[0, 1] / np.sqrt(covariance[0, 0] * covariance[1, 1])
+        ell_radius_x = np.sqrt(1 + pearson)
+        ell_radius_y = np.sqrt(1 - pearson)
+        scale_y = np.sqrt(covariance[1, 1]) * n_std
+        scale_x = np.sqrt(covariance[0, 0]) * n_std
+        ellipse = Ellipse(centroid, width=ell_radius_x * scale_x, height=ell_radius_y * scale_y, facecolor=group_color, edgecolor=group_color, ls='--', lw=0.8, alpha=0.1)
+        ellipse_z = objective.evaluate_visual(centroid)
+
+        axis.add_patch(ellipse)
+        art3d.pathpatch_2d_to_3d(ellipse, z=ellipse_z, zdir='z')
+
+    return centroid, covariance
